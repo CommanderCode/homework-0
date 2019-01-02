721ef1fdfbc91a3d89ca5d5c954d62b9c783d38a
CommanderCode <ahaas08@gmail.com>
2019-01-02 15:02:25 +0900

    Add files via upload

---
+++ movielens predictions.R
@@ -0,0 +1,88 @@
+#############################################################
+# Create edx set, validation set, and submission file
+#############################################################
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+
+ratings <- read.table(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+                      col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Validation set will be 10% of MovieLens data
+
+set.seed(1)
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in validation set are also in edx set
+
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+# Learners will develop their algorithms on the edx set
+# For grading, learners will run algorithm on validation set to generate ratings
+
+validation <- validation %>% select(-rating)
+
+# Ratings will go into the CSV submission file below:
+
+rm(dl, ratings, movies, test_index, temp, movielens, removed)
+
+# Ratings will go into the CSV submission file below:
+
+
+##########################################################################
+#  There was a lot of data exploration, methods, and analysis 
+#   in my report, but I will only 
+#    include the code that produce my predictions here.
+
+#Get the average of all user ratings, save as mu
+mu <- mean(edx$rating)
+
+#  Find the movie effect, b_i :  Get all movie averages and center at mu
+movie_avgs <- edx %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = mean(rating - mu))
+
+#  Find the user effect, b_u :  get all user averages and center at mu-b_i
+user_avgs <- edx %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+#  Make predictions of all entries in the validation set using mu + b_i + b_u
+predicted_ratings<- validation %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  .$pred
+# round all predictions to nearest 0.5
+predicted_ratingsr <- round(predicted_ratings/0.5)*.5
+
+
+write.csv(validation %>% select(userId, movieId) %>% mutate(rating = predicted_ratingsr),
+          "submission.csv", na = "", row.names=FALSE)
